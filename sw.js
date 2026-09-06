be0a05ed9cbe5993ce6fb8f670243b2d42072eb7
amin-nepali <backup.9811003628@gmail.com>
2026-09-06 23:06:05 +0545

    dfghjkk

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "proofly-shell-v19";
+const CACHE_NAME = "proofly-shell-v20";
 const BASE_PATH = new URL("./", self.registration.scope).pathname;
 const APP_SHELL = [
   BASE_PATH,
